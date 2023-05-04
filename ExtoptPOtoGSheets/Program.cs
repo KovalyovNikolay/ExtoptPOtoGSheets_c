cf241a051a36d09908ec3880efcb74359ae24b20
KovalyovNikolay <dvrmail.7@gmail.com>
2023-05-04 15:06:24 +0600

    Test console output info

--- ExtoptPOtoGSheets/Program.cs
+++ ExtoptPOtoGSheets/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ExtoptPOtoGSheets
 {
@@ -10,6 +11,9 @@ namespace ExtoptPOtoGSheets
     {
         static void Main(string[] args)
         {
+            string testPO_adr = @"D:\Game.po"; // add test info
+            string testAPI = "MYgoogleSPREADSHEETapiEXAMPLE"; // add test api string
+            // test string needs to check output
             /*
              Example
             start
@@ -22,6 +26,14 @@ namespace ExtoptPOtoGSheets
             GetData() {}
             ExportData() {}
              */
+            Console.WriteLine("DUser. Please enter data:");
+            Console.WriteLine("");
+            Console.WriteLine("Your API is: '{0}'", testAPI);
+            Console.WriteLine("Your PO file name is: '{0}'", Path.GetFileName(testPO_adr));
+            Console.WriteLine("Your PO file path is: '{0}'", Path.GetDirectoryName(testPO_adr));
+            Console.ReadKey();
         }
+        //private void SetData() { }
+
     }
 }
